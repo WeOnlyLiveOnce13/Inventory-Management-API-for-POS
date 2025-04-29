421a8b5c7d1b242fb2edd8c8b28cb1343409da46
DAN AKULAYI TSHISUNGU <dantshisungu@gmail.com>
2025-04-29 11:16:06 +0200

    first commit of Starter project

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from "express";
+
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+app.listen(PORT, () => {
+  
+  console.log(`Server is running on http://localhost:${PORT}`); 
+});
+
+// Create an API endpoint to get all customers
+// http:localhost:8000/customers
+app.get("/customers", async (req: Request, res: Response) => {
+  const customers = [
+    { name: "John Doe", email: "john.doe@example.com", phone: "+1234567890" },
+    {
+      name: "Joel Smith",
+      email: "joel.smith@example.com",
+      phone: "+0987654321",
+    },
+    {
+      name: "Muke John",
+      email: "john.muke@example.com",
+      phone: "+0987654321",
+    },
+  ];
+
+  return res.status(200).json(customers);
+});
